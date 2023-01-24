ca60918ee5eed3c2f7867ef8edca431284a0021b
RobertKaleta <95703016+RobertKaleta@users.noreply.github.com>
2023-01-24 16:55:49 +0100

    Update Search.js

--- screens/Search.js
+++ screens/Search.js
@@ -1,11 +1,50 @@
-import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
+import { View, StyleSheet, Text, TextInput,ScrollView,Image} from "react-native";
 
 const Search = () => {
+   const apiurl="https://api.themoviedb.org/3/search/movie?api_key=8339d23b0d359649d6e79b1aad386819";
+   const API_IMG = "https://image.tmdb.org/t/p/w500";
+   const [success,setSuccess] = useState(false);
+   const [state,setState] = useState({
+   s: "Podaj film...",
+   results:[],
+   });
+
+   const search = () => {
+       axios(apiurl + "&query="+ state.s).then(({ data }) => {
+           let results = data.results;
+           console.log(data);
+           setState(prevState => {
+               return{...prevState, results: results }
+           })
+           setSuccess(true);
+       })
+       }
   return (
-        <Text>Ekran wyszukiwania</Text>
+         <View>
+         <TextInput
+            onChangeText={text => setState(prevState => {
+            return {...prevState, s:text}
+            })}
+            onSubmitEditing={search}
+            value={state.s}
+         />
+        <ScrollView>
+                    {success && state.results.slice(0,3).map(result =>(
+                        <View key={result.id}>
+                            <Text>{result.title}</Text>
+                            <Image source={{
+                                        uri: API_IMG + result.poster_path,
+                                        }}
+                                        style={styles.tinyLogo}
+                                        />
+                        </View>
+                    ))}
+                </ScrollView>
+        </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   center: {
@@ -14,6 +53,10 @@ const styles = StyleSheet.create({
     alignItems: "center",
     textAlign: "center",
   },
+  tinyLogo: {
+      width: 100,
+      height: 140,
+    },
 });
 
 export default Search;
