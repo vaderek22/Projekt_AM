ad21f23ad8557957df1728ce2b889a13fb142f9d
RobertKaleta <95703016+RobertKaleta@users.noreply.github.com>
2023-01-24 17:02:49 +0100

    Update Cinema.js

--- screens/Cinema.js
+++ screens/Cinema.js
@@ -4,10 +4,10 @@ import { StyleSheet, View } from 'react-native';
 
 export default function Cinema() {
   const [mapRegion,setMapRegion] = useState({
-  latitude: 50.8833,
-  longitude: 20.6167,
+  latitude: 50.87894787774452,
+  longitude: 20.640055698318204,
   latitudeDelta: 0.1,
-  longitudeDelta:0.1,
+  longitudeDelta: 0.1,
   });
   return (
     <View style={styles.container}>
@@ -22,7 +22,17 @@ export default function Cinema() {
                         latitude:50.875787844438136,
                         longitude:20.635209082079406,
               }} title='Multikino' description='Warszawska 26, 25-312 Kielce'>
-               </Marker>
+        </Marker>
+        <Marker coordinate={{
+                        latitude: 50.871175751548925,
+                        longitude: 20.626349900121188,
+              }} title='Kino Moskwa' description='Księdza Stanisława Staszica 5, 25-008 Kielce'>
+        </Marker>
+        <Marker coordinate={{
+                        latitude: 50.86416087695226,
+                        longitude: 20.626864884255617,
+              }} title='Kino Fenomen' description='Księdza Piotra Ściegiennego 2, 25-033 Kielce'>
+        </Marker>
       </MapView>
     </View>
   );
