da24b03bd77db926c94fd659c8a8df0c155450bf
vaderek22 <42940388+vaderek22@users.noreply.github.com>
2023-01-26 11:59:25 +0100

    Update Home.js

--- screens/Home.js
+++ screens/Home.js
@@ -1,17 +1,65 @@
-import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { useNavigation } from '@react-navigation/native';
+import { ScrollView, View, Text, Image, StyleSheet, TouchableOpacity} from "react-native";
+import axios from "axios";
+import {API_URLD} from '../control/consts';
+import {API_IMG} from '../control/consts';
 const Home = () => {
+const [movies, setMovies] = useState([]);
+const navigation = useNavigation();
+useEffect(() => {
+    const fetchData = async () => {
+        try {
+            const response = await axios.get(API_URLD);
+            setMovies(response.data.results.slice(0, 10));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+}, []);
   return (
-              <Text style={styles.center}>Ekran główny</Text>
+    <View style={styles.center}>
+    <Text style={styles.Text}>Witaj, sprawdź nasze propozycje na dziś</Text>
+     <ScrollView horizontal={true}>
+          {movies.map((movie, index) => (
+            <View key={index} style={styles.movieContainer}>
+              <TouchableOpacity key={movie.id} onPress={() => navigation.navigate('MovieDetails', { movie: movie })}>
+              <Image
+                source={{
+                  uri: API_IMG + movie.poster_path,
+                }}
+                style={styles.moviePoster}
+              />
+              </TouchableOpacity>
+            </View>
+          ))}
+        </ScrollView>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   center: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    textAlign: "center",
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        textAlign: "center",
+        paddingLeft: 15,
+  },
+  Text: {
+       color: 'black',
+       fontSize: 14,
+       fontFamily:'serif',
+       paddingTop:30,
+       paddingBottom:30,
+  },
+  movieContainer: {
+       marginRight: 10,
+  },
+  moviePoster: {
+       width: 250,
+       height: 400,
   },
 });
 
